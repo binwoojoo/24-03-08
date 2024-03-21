392f8f09d08a2551afbaf1cbc6841cfc82bfde4f
binwoojoo <oooo940212@gmail.com>
2024-03-21 17:45:18 +0900

    quiz

--- Javascript/1.core javascript/2. 제어문/7. 중첩 loop .js
+++ Javascript/1.core javascript/2. 제어문/7. 중첩 loop .js
@@ -15,6 +15,6 @@ console.log(`# 구구단 ${level}단`);
 for (var line = 1; line <= 9; line++) {
   console.log(`-> ${level} x ${line} = ${level * line}`);
 }
-}
 console.log('======================');
+}
 
--- Javascript/1.core javascript/2. 제어문/quiz/7. loop_nesting_quiz .js
+++ Javascript/1.core javascript/2. 제어문/quiz/7. loop_nesting_quiz .js
@@ -2,13 +2,3 @@ var w = 4;
 var h = 7;
 var result = ''
 
-for(x = 1; x <= h; x++){
-  result += `${x}\n`;
-  console.log(result);
-  for(y = 1; y <= w; y++){
-    result += `${y}`;
-    
-  }
-  console.log(result);
-}
-
