f93d598187bb5a2a87c9eba1f6a0eb6e6bd2141a
binwoojoo <oooo940212@gmail.com>
2024-03-22 17:12:16 +0900

    4math quiz

--- Javascript/1.core javascript/2. 제어문/quiz/9. 4math quiz .js
+++ Javascript/1.core javascript/2. 제어문/quiz/9. 4math quiz .js
@@ -4,7 +4,6 @@ var lose = 0;
 const levelHigh = 1, levelMid = 2, levelLow = 3;
 level = +prompt('난이도를 선택하세요\n1.난이도 상(1~100) 2.난이도 중(1~50) 3.난이도 하(1~20')
 
-
 while(true) {
   if(level === levelHigh){
   var secret1 = Math.floor(Math.random() * 100) + 1;
@@ -18,7 +17,8 @@ while(true) {
       var secret1 = Math.floor(Math.random() * 20) + 1;
       var secret2 = Math.floor(Math.random() * 20) + 1;
       }
-  
+  // - x 경우의 수 만들기 
+
   var answer = +prompt(`문제를 풀다가 지겨우면 0을 누르세요ㅋㅋ\nQ${game}. ${secret1} + ${secret2} = ??` )
 
   if(secret1 + secret2 === answer) {
@@ -31,4 +31,5 @@ while(true) {
   if(answer === 0) {
     alert(`ㄲㅈ\n정답 횟수: ${win} 오답 횟수:${lose}\n게임 종료`); break 
   }
+
 }
