b928f8cd4e34e50aa825ab8ec4926cd73b92c286
oooowb <oooo940212@gmail.com>
2024-03-28 10:12:11 +0900

    배열 고차함수

--- Javascript/1.core javascript/8. ES6 .js/quiz/quiz2 .js
+++ Javascript/1.core javascript/8. ES6 .js/quiz/quiz2 .js
@@ -68,7 +68,7 @@ function some(callback2) {
 }
 
 function every(callback3) {
-  let checker = false;
+  let checker = null;
   for (const user of userList) {
     if (callback3(user)) {
       checker = true;
@@ -81,14 +81,14 @@ function every(callback3) {
 }
 
 // // console.log(find());
-// console.log("최소 salary를 가진 사용자:", min(userList, "salary"));
-// console.log("최소 age를 가진 사용자:", min(userList, "age"));
+console.log("최소 salary를 가진 사용자:", min(userList, "salary"));
+console.log("최소 age를 가진 사용자:", min(userList, "age"));
 
-// // '서울'에 사는 첫 번째 사용자를 찾는 예시
-// const firstUserInSeoul = find(function (user) {
-//   return user.address === "서울";
-// });
-// console.log("서울에 사는 첫 번째 사용자:", firstUserInSeoul);
+// '서울'에 사는 첫 번째 사용자를 찾는 예시
+const firstUserInSeoul = find(function (user) {
+  return user.address === "서울";
+});
+console.log("서울에 사는 첫 번째 사용자:", firstUserInSeoul);
 
 // // 급여 5,000,000 이상인 사용자가 하나라도 있는지 확인하는 예시
 const hasHighEarningUser = some((user) => user.salary >= 5000000);
